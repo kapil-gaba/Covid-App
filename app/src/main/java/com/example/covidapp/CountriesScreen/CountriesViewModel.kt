8be8b0ee8af8b204ba6a3605cb81f6131aa8b931
Kapil <kgaba.lmach@gmail.com>
2020-06-26 01:53:00 +1000

    Initial Commit

---
+++ app/src/main/java/com/example/covidapp/CountriesScreen/CountriesViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.covidapp.CountriesScreen
+
+import androidx.lifecycle.ViewModel
+
+class CountriesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/covidapp/GlobalScreen/GlobalViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.covidapp.GlobalScreen
+
+import androidx.lifecycle.ViewModel
+
+class GlobalViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
